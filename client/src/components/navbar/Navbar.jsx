651ce8e65fd308a7b09baa3402edae4d232f8301
Shorya-Dixit <shorya.dixit2002@gmail.com>
2024-07-29 13:59:43 +0530

    Initial commit

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,66 @@
+import { useContext, useState } from "react";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import { useNotificationStore } from "../../lib/notificationStore";
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
+
+  const fetch = useNotificationStore((state) => state.fetch);
+  const number = useNotificationStore((state) => state.number);
+
+  if(currentUser) fetch();
+
+  return (
+    <nav>
+      <div className="left">
+        <Link to="/" className="logo">
+          <img src="/Logo1.png" alt="" />
+        </Link>
+        <Link to="/" className="navLinks">Home</Link>
+        <Link to="/" className="navLinks">About</Link>
+        <Link to="/" className="navLinks">Contact</Link>
+        <Link to="/" className="navLinks">Agents</Link>
+      </div>
+      <div className="right">
+        {currentUser ? (
+          <div className="user">
+            <img src={currentUser.avatar || "/noavatar.jpg"} alt="" />
+            <span id="username">{currentUser.username}</span>
+            <Link to="/profile" className="profile">
+              {number > 0 && <div className="notification">{number}</div>}
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <Link to="/login">Sign in</Link>
+            <Link to="/register" className="register">
+              <span>Sign up</span>
+            </Link>
+          </div>
+        )}
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <Link to="/">Home</Link>
+          <Link to="/">About</Link>
+          <Link to="/">Contact</Link>
+          <Link to="/">Agents</Link>
+          <Link to="/login">Sign in</Link>
+          <Link to="/register">Sign up</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,42 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title"><p>Find a home you'll </p><span> love</span><img src="/love.png" alt="" /></h1>
+          <p className="desc">
+          We've got properties in India for everyone. You'll get the best prices here.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/home2.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
